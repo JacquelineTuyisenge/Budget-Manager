8e3a8f52e2b66de9f6e60b1be24cb877526367b7
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-09-28 16:25:27 +0200

    added functionalities to personal budget manager

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import ExpenseForm from "./components/ExpenseForm";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Provider store={store}>
         <Nav />
         <ExpenseForm />
+        <Footer />
     </Provider>
   );
 }
